f160590b3f1292158a2afa91db5c84a5969d9729
ClassicalJo <jb.barrientos@yahoo.com>
2020-01-09 17:01:16 +1100

    Updates

---
+++ precache-manifest.b86710af8b9986388359e4d77ba6dbf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab68cd370c3412fcea734690b9a3ff4d",
+    "url": "classicaljo.github.io/blinkjs/index.html"
+  },
+  {
+    "revision": "18e8923d288ec46d7935",
+    "url": "classicaljo.github.io/blinkjs/static/css/main.f3601fba.chunk.css"
+  },
+  {
+    "revision": "a26e07d993ce42c6a45b",
+    "url": "classicaljo.github.io/blinkjs/static/js/2.c989a929.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "classicaljo.github.io/blinkjs/static/js/2.c989a929.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18e8923d288ec46d7935",
+    "url": "classicaljo.github.io/blinkjs/static/js/main.b1403b49.chunk.js"
+  },
+  {
+    "revision": "655039cb50d7e33846de",
+    "url": "classicaljo.github.io/blinkjs/static/js/runtime-main.f21d0a1b.js"
+  }
+]);
