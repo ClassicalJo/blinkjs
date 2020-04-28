f0829c55b8c7f4875c2994982e881e0eca3a2d69
ClassicalJo <jb.barrientos@yahoo.com>
2020-04-28 23:28:58 +1000

    small duplicate attribute fix

--- src/scenes/Controller.js
+++ src/scenes/Controller.js
@@ -5,7 +5,7 @@ import Keyboard from '../assets/images/keyboard.svg'
 let Controller = (props) => {
     return (
         <Viewbox>
-            <rect strokeWidth="5" stroke="rgba(255,255,255,0.2)" stroke="rgba(255,255,255,0.2)" fill="black" x={-600} y={-450} width={1200} height={900} onClick={() => props.sceneChange("start")} />
+            <rect strokeWidth="5" stroke="rgba(255,255,255,0.2)"  fill="black" x={-600} y={-450} width={1200} height={900} onClick={() => props.sceneChange("start")} />
             <image width="250" x={-125} y={-250 + 250 / -2} xlinkHref={Keyboard} pointerEvents="none" />
             <text fontFamily="Arial Black" textAnchor='middle' x={0} y={100} fill="white" pointerEvents="none">WASD: Movement</text>
             <text fontFamily="Arial Black" textAnchor='middle' x={0} y={150} fill="white" pointerEvents="none">K: Blink</text>
